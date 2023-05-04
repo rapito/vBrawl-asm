16d9375335f3cde760ada56afb917a8dc88caccc
Robert Peralta <rapito@gmail.com>
2023-05-04 15:07:15 -0400

    set project on cmakelist to avoid doing it on shell script

--- build-macos.sh
+++ build-macos.sh
@@ -2,8 +2,7 @@
 
 # Move into the build directory, run CMake, and compile the project
 mkdir -p build
-cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=./CMake/PPCToolchain.cmake -DBRAWLBACK=1
-#cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=./CMake/PPCToolchain.cmake -DBRAWLBACK=1
+cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=./CMake/PPCToolchain.cmake
 cmake --build build/ -- -j $(sysctl -n hw.logicalcpu)
 
 source ./sdcard-macos.sh
