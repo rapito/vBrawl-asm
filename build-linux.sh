807965a6fd51174931ab8874fcc1ee96931889fb
Robert Peralta <rapito@gmail.com>
2023-05-04 15:03:29 -0400

    Add build-linux.sh

---
+++ build-linux.sh
@@ -0,0 +1,8 @@
+#bin/sh
+
+# Move into the build directory, run CMake, and compile the project
+mkdir -p build
+cmake -B build/ -S . -DCMAKE_TOOLCHAIN_FILE=./CMake/PPCToolchain.cmake
+cmake --build build/ -- -j $(sysctl -n hw.logicalcpu)
+
+#source ./sdcard-macos.sh
