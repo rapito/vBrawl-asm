303b3cf65a22b491781c6681cbfb4bd806ebd0ca
Robert Peralta <rapito@gmail.com>
2023-05-04 22:03:38 -0400

    test sd

--- sdcard-linux.sh
+++ sdcard-linux.sh
@@ -8,8 +8,8 @@ cd "$(dirname "$0")"
 
 VOLUME_NAME="BrawlbackSDCard"
 MOUNT_DRIVE="/mnt/$VOLUME_NAME"
-SD_CARD1_PATH="./sdcard1"
-SD_CARD2_PATH="./sdcard2"
+SD_CARD1_PATH="./sdcard1.raw"
+SD_CARD2_PATH="./sdcard2.raw"
 
 # Define the relative path of the folder to copy to the SD card.
 SD_CARD_FOLDER="./SDCard"
@@ -26,24 +26,25 @@ for sd_card_path in "${SD_CARDS[@]}"; do
     echo "Mounting existing SD card: $sd_card_path"
   else
     echo "Creating new SD card: $sd_card_path"
-#    sudo dd if=/dev/zero of="$sd_card_path.raw" bs=1M count="$SD_CARD_SIZE"
-    sudo dd if=/dev/zero of="$sd_card_path.raw" bs=1 count=0 seek="$SD_CARD_SIZE"
-    sudo mkfs.fat -n "$VOLUME_NAME" -F32 "$sd_card_path.raw"
+#    dd if=/dev/zero of="$sd_card_path" bs=1M count="$SD_CARD_SIZE"
+    dd if=/dev/zero of="$sd_card_path" bs=1 count=0 seek="$SD_CARD_SIZE"
+    mkfs.fat -F 32 "$sd_card_path"
   fi
 
   # Mount the SD card image.
-  echo "Mounting SD card: $sd_card_path.raw"
-  sudo mkdir -p "$MOUNT_DRIVE"
-  sudo mount -o loop "$sd_card_path.raw" "$MOUNT_DRIVE"
+  echo "Mounting SD card: $sd_card_path"
+  mkdir -p "$MOUNT_DRIVE"
+  mount -o loop "$sd_card_path" "$MOUNT_DRIVE"
 
   # Copy the contents of the specified folder to the SD card.
   echo "Copying files to SDCard: $SD_CARD_FOLDER => $MOUNT_DRIVE"
-  sudo cp -R "$SD_CARD_FOLDER/*" "$MOUNT_DRIVE"
+  cp -R "$SD_CARD_FOLDER"/* "$MOUNT_DRIVE"
 
   # Unmount the SD card image.
   echo "Unmounting SD card: $MOUNT_DRIVE"
-  sudo umount "$MOUNT_DRIVE"
+  umount "$MOUNT_DRIVE"
 
+  gzip "$sd_card_path"
 done
 
 # Restore the current directory.
